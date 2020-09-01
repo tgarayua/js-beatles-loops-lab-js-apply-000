ef639dafb9acf2b1b5e6724c2b1c841b8e8a31b3
Thomas Garayua <tgarayua@gmail.com>
2020-09-01 01:48:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
-  var x = [];
+  var arr = [];
   for (let i = 0; i < musicians.length; i++) {
-    x.push(`${musicians[i]} plays ${instruments[i]}`);
+    arr.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return x;
 }
